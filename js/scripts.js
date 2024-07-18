7b206032a67c8921d6fe60a3849ec8a9572a0f18
Junior Mabunda <mjmabunda0@gmail.com>
2024-07-18 08:12:54 +0200

    Add random color generator with progressive darkening.
    Each square on the grid gets a random color when hovered over to make things fun.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,31 @@
 let container = document.querySelector('#container');
 let width = 16;
 let height = 16;
+const LIMIT = 100;
+let alpha = 0;
 
-function createGrid(container, width, height) {
+function getRandomRGB() {
+    let R = Math.round(Math.random() * 255);
+    let G = Math.round(Math.random() * 255);
+    let B = Math.round(Math.random() * 255);
+
+    let randomColor = {red: R, green: G, blue: B, alpha: alpha};
+    if (alpha < 1) {
+        return darkenColor(randomColor);
+    }
+    return randomColor;
+}
+
+function darkenColor(color) {
+    // Darken by 10%
+    alpha += 0.1;
+    color.alpha = alpha;
+    return color;
+}
+
+function createGrid(width, height) {
+    clearGrid();
+
     for (let i = 0; i < height; i++) {
         // Create row and add to container
         let row = document.createElement('div');
@@ -15,13 +38,39 @@ function createGrid(container, width, height) {
 
             // Draw on hover
             square.addEventListener('mouseover', (event) => {
-                event.target.style.backgroundColor = 'black';
+                let color = getRandomRGB();
+                color = `rgba(${color.red}, ${color.green}, ${color.blue}, ${color.alpha})`;
+                event.target.style.backgroundColor = color;
             });
 
             row.appendChild(square);
         }
         container.appendChild(row);
     }
+
+    document.querySelector('.dimensions').textContent =
+        `${width} x ${height}`;
 }
 
-createGrid(container, width, height);
+function clearGrid() {
+    while (container.childNodes.length > 0) {
+        container.removeChild(container.childNodes[0]);
+    }
+}
+
+document.querySelector('#new-grid-button').addEventListener('click', () => {
+    let new_dimensions = 
+        Number(prompt('Enter the new dimensions (max 100):'));
+    console.log(new_dimensions);
+    if (new_dimensions > LIMIT ||
+        new_dimensions == null||
+        new_dimensions == NaN ||
+        new_dimensions <= 0) {
+        alert('Enter a value between 1 and 100.');
+        return;
+    }
+
+    createGrid(new_dimensions, new_dimensions);
+});
+
+createGrid(width, height);
